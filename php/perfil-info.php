08c63aa3fdbd9dfd81a6ab0e9fdb579ad1df3f16
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-11-13 14:05:07 -0500

    Logoin y Logout

--- php/perfil-info.php
+++ php/perfil-info.php
@@ -34,7 +34,6 @@ if (!isset($_SESSION["datos"])) {
             echo "<img class='h-8 w-8'  style='width: 2rem' src='data:image/jpg; base64," . base64_encode($_SESSION["dato_imagen"]) . "'>";
             ?>
         </div>
-
             <p class="list-popover"> &#160; <?php echo $_SESSION["dato_nombre"] ?></p>
             <button onclick="popoverSpin()">
                 <svg id="triangulito-spin" style="transition-duration: 500ms;" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="-8 0 30 10">
@@ -69,16 +68,11 @@ if (!isset($_SESSION["datos"])) {
         </div>
         </div>
     </nav>
-
-
 <div id="formularioInfo">
-
 <form id="table-info" class="w-full " action="update_data.php">
         <h1 class="mb-2 mt-4 colorPar">Personal info</h1>
         <p class="mb-6 colorPar">Basic info, like your name and photo</p>
-
         <div id="conInfoForm"  class="border-solid border-2">
-
             <div class="px-6 md:px-20 h-24 w-full flex justify-between items-center border-b border-gray">
                 <div>
                     <p class="text-lg">Profile</p>
@@ -96,28 +90,31 @@ if (!isset($_SESSION["datos"])) {
                 </div>
             </div>
 
-            <div class="flex inputData">
+            <div class="inputData">
                 <p class=" flex items-center">NAME</p>
+                <div>
                 <input 
                 class="input-form"
                 type="text" 
                 value="<?php echo $_SESSION["dato_nombre"] ?>" 
                 size="20" 
                 disabled />
+                </div>
             </div>
-
-            <div class="flex inputData">
+            <div class="inputData">
                 <p class="w-2/6 h-full flex items-center">BIO</p>
+                <div>
                 <input 
                 class="input-form"
                 type="text" 
                 value="<?php echo $_SESSION["dato_bio"] ?>" 
                 disabled 
                 />
+                </div>
             </div>
-
-            <div class="flex inputData">
+            <div class="inputData">
                 <p class="w-2/6 h-full flex items-center">PHONE</p>
+                <div>
                 <input 
                 class="input-form"
                 type="text" 
@@ -125,10 +122,11 @@ if (!isset($_SESSION["datos"])) {
                 size="20" 
                 disabled
                 />
+                </div>
             </div>
-
-            <div class="flex inputData" >
+            <div class="inputData" >
                 <p class="w-2/6 h-full flex items-center">EMAIL</p>
+                <div>
                 <input 
                 class="input-form" 
                 type="text" 
@@ -136,18 +134,20 @@ if (!isset($_SESSION["datos"])) {
                 placeholder="Escriba su email" 
                 disabled 
                 />
+                </div>
             </div>
-
-            <div class="flex inputData">
+            <div class="inputData">
                 <p class="w-2/6 h-full flex items-center">PASSSWORD</p>
+                <div>
                 <input 
                 class="input-form"
                 type="password" 
-                value="<?php echo $_SESSION["dato_passw"] ?>" 
+                value="************" 
                 placeholder="Escriba su password" 
                 size="20" 
                 disabled
                 />
+                </div>
             </div>
         </div>
         <div id="contenedor-footer">
@@ -156,60 +156,12 @@ if (!isset($_SESSION["datos"])) {
         </div>
 
     </form>
-
-
     </div>
-
-
-
-
 </body>
-
 <script>
     function ejecutarCambiarModo() {
     textModel()
     cambiarModo();  
     }
 </script>
-
 </html>
-
-
-<!-- <body>
-    <div id="container-1" class="border-solid border-2">
-        <div id="contenedor-2">
-            <img id="logo" alt="devchallenges.png"/>
-            <br>
-            <h2 class="text-xl font-semibold">Join thousands of learners from around the world</h2><br>
-            <h3 class="text-sm">Master web development by making real-life projects. There are multiple paths for
-                you to choose</h3>
-        </div>
-        <form id="form-1" action="./php/login-db.php" method="post" enctype="multipart/form-data">
-            <i class="fa-solid fa-envelope fa-xl"></i>
-            <input class="user-name  text-gray-700" type="text" name="email" placeholder="Email" required/>
-            <i class="fa-solid fa-lock fa-xl"></i>
-            <input class="user-name" type="password" name="passw" placeholder="Password" required/>
-            <p class="text-xs p-2  text-red-700">
-            </p>
-            <button type="submit" id="button-log">Star coding now</button>
-        </form>
-        <div id="container-3">
-            <p>or continue with these social profile</p>
-        </div>
-        <div id="container-btn" class="flex">
-            <a href="#"><img src="./assets/google.svg" alt="google.svg" /></a>
-            <a href="#"><img src="./assets/Facebook.svg" alt="Facebook.svg" /></a>
-            <a href="#"><img src="./assets/Twitter.svg" alt="Twitter.svg" /></a>
-            <a href="#"><img src="./assets/Gihub.svg" alt="Gihub.svg" /></a>
-        </div>
-        <div id="contenedor-4">
-            <p>Already a member? </p>
-            <p><a href="./login.php">&#160;Login</a></p>
-        </div>
-        <button id="switch"></button>
-        <div id="contenedor-footer">
-            <p>create by <a class="text-footer" href="https://github.com/BrandonRv">BrandonRv</a></p>
-            <p>devChallenges.io</p>
-        </div>
-    </div>
-</body> -->
